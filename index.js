5dd15d84777bf1717971b9af74b5f74f04cd9eae
pavanKillada <killadapavankumar@gmail.com>
2023-02-24 15:12:25 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const element = (
+    <h1>Super Over League</h1>;
+    <div className = "imgs-container">
+        <img src = "https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="imgs"/>
+        <img src = "https://assets.ccbp.in/frontend/react-js/csk-img.png" className="imgs"/>
+    </div>;
+);
+
+ReactDOM.render(element, document.getElementById('root'));

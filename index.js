043a54c066f712d464ee2520f2679fa10f87e8ac
pavanKillada <125582049+pavanKillada@users.noreply.github.com>
2023-02-25 15:21:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const element = (
-    <h1>Super Over League</h1>
-    <div className = "imgs-container">
-        <img src = "https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="imgs"/>
-        <img src = "https://assets.ccbp.in/frontend/react-js/csk-img.png" className="imgs"/>
+    <div>
+        <h1>Super Over League</h1>
+        <div className = "imgs-container">
+            <img src = "https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="imgs"/>
+            <img src = "https://assets.ccbp.in/frontend/react-js/csk-img.png" className="imgs"/>
+        </div>
     </div>
 );
 
